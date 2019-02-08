bcbe2c5acf465bf59e6449dd88de5bfcaf597d33
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-02-08 11:46:22 +0400

    Updates

---
+++ precache-manifest.8af4ca17274acadd4caab92e60d9b8ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a93694d5e30917eee4",
+    "url": "/learn-alphabet/static/js/runtime~main.43a93694.js"
+  },
+  {
+    "revision": "1b7e2cebcdd71d616496",
+    "url": "/learn-alphabet/static/js/main.1b7e2ceb.chunk.js"
+  },
+  {
+    "revision": "7146b63508beb7feb788",
+    "url": "/learn-alphabet/static/js/1.7146b635.chunk.js"
+  },
+  {
+    "revision": "1b7e2cebcdd71d616496",
+    "url": "/learn-alphabet/static/css/main.53e23295.chunk.css"
+  },
+  {
+    "revision": "75687063dfd7e98bdd1921e11607cd36",
+    "url": "/learn-alphabet/index.html"
+  }
+];
